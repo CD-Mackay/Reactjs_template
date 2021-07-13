e6ceb4864d4e6ac4aeb60e9229ac24fa68681e29
Connor Mackay <connor.mackay@gmail.com>
2021-07-13 12:01:00 -0400

    IMP: Webpack and Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,46 @@
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 const htmlPlugin = new HtmlWebPackPlugin({
  template: "./src/index.html",
  filename: "./index.html"
 });
+
 module.exports = {
-mode: 'development',
+  entry: "./src",
+  output: {
+    path: path.resolve(__dirname, "/"),
+    filename: "bundle.js",
+    publicPath: '/',
+  },
   module: {
-    rules: [{
-   test: /\.js$/,
-   exclude: /node_modules/,
-   use: {
-     loader: "babel-loader"
-   }
- },
-  {
-   test: /\.css$/,
-   use: ["style-loader", "css-loader"]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: { dataUrlCondition: { maxSize: 15000 } },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+      elements: path.resolve(__dirname, './src/components/elements'),
+      modules: path.resolve(__dirname, './src/components/modules'),
+      templates: path.resolve(__dirname, './src/components/templates'),
+      pages: path.resolve(__dirname, './src/components/pages')
+    },
+    extensions: ["*", ".js", ".jsx", '.scss', '.css'],
+  },
+  plugins: [
+    htmlPlugin
+  ],
+  devServer: {
+    historyApiFallback: true
   }
-]},
- plugins: [htmlPlugin]
 };
